903c5214a3053f85c9601f71afa7b4bda9a51aa6
lqg <1@qq.com>
2022-04-01 20:15:47 +0800

    v1

---
+++ src/main/java/it/com/GitDomeDallon.java
@@ -0,0 +1,13 @@
+package it.com;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class GitDomeDallon {
+    public static void main(String[] args) {
+        SpringApplication.run(GitDomeDallon.class);
+    }
+}
---
+++ src/main/java/it/com/controller/HelloController.java
@@ -0,0 +1,14 @@
+package it.com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "<h1>hello</h1>";
+    }
+}
